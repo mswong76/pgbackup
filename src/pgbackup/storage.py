c37b990488e4ce0c78b4d4a800eb1dfc8132c2ce
Micah Wong <mswong76@gmail.com>
2018-02-22 00:41:25 -0800

    Implement AWS S3

--- src/pgbackup/storage.py
+++ src/pgbackup/storage.py
@@ -2,3 +2,6 @@ def local(infile, outfile):
 	outfile.write(infile.read())
 	outfile.close()
 	infile.close()
+
+def s3(client, infile, bucket, file_name):
+	client.upload_fileobj(infile, bucket, file_name)
