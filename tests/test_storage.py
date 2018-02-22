e7621fe4aaf1fb4004e13874d4df97d6554ab680
Micah Wong <mswong76@gmail.com>
2018-02-22 00:04:14 -0800

    Implement local storage

---
+++ tests/test_storage.py
@@ -0,0 +1,15 @@
+import tempfile
+from pgbackup import storage
+
+def test_storing_file_locally():
+	"""
+	Write content from one file-like to another
+	"""
+	infile = tempfile.TemporaryFile('r+')
+	infile.write('Testing')
+	infile.seek(0)
+
+	outfile = tempfile.NamedTemporaryFile(delete=False)
+	storage.local(infile, outfile)
+	with open(outfile.name) as file:
+		assert file.read() == 'Testing'
